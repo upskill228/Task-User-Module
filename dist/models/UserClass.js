7e2323ae5d8ddb97e34ecf15d0f5774eb2928b69
upskill228 <upskill228@alunos.ciencias.ulisboa.pt>
2026-01-26 23:11:21 +0000

    casa

---
+++ dist/models/UserClass.js
@@ -0,0 +1,30 @@
+import { BaseEntity } from "./BaseEntity.js";
+export class UserClass extends BaseEntity {
+    constructor(id, email, role, active = true) {
+        super(id);
+        this.setEmail(email);
+        this.role = role;
+        this.active = active;
+    }
+    // GETTERS
+    getEmail() {
+        return this.email;
+    }
+    isActive() {
+        return this.active;
+    }
+    getRole() {
+        return this.role;
+    }
+    // SETTERS (com validação)
+    setEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            throw new Error("Email inválido");
+        }
+        this.email = email;
+    }
+    toggleActive() {
+        this.active = !this.active;
+    }
+}
---
+++ src/models/UserClass.ts
@@ -0,0 +1,47 @@
+import { BaseEntity } from "./BaseEntity.js";
+import { UserRole } from '../security/UserRole.js';
+
+export class UserClass extends BaseEntity {
+    private email!: string;
+    private active: boolean;
+    private role: UserRole;
+
+    constructor(
+        id: number,
+        email: string,
+        role: UserRole,
+        active = true
+    ) {
+        super(id);
+        this.setEmail(email);
+        this.role = role;
+        this.active = active;
+    }
+
+    // GETTERS
+    getEmail(): string {
+        return this.email;
+    }
+
+    isActive(): boolean {
+        return this.active;
+    }
+
+    getRole(): UserRole {
+        return this.role;
+    }
+
+    // SETTERS (com validação)
+    setEmail(email: string): void {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            throw new Error("Email inválido");
+        }
+        this.email = email;
+    }
+
+    toggleActive(): void {
+        this.active = !this.active;
+    }
+
+}
