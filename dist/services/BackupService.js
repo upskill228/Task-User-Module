73b693132eb29979e306029e7541fa22e2d19e0c
upskill228 <upskill228@alunos.ciencias.ulisboa.pt>
2026-01-29 22:17:27 +0000

    casa

---
+++ dist/services/BackupService.js
@@ -0,0 +1,25 @@
+import { getTaskList } from "./taskServiceNew.js";
+import { getUsersFromTask } from "./AssignmentService.js";
+export function exportTasks() {
+    return getTaskList().map(task => (Object.assign({}, task)));
+}
+export function exportUsers() {
+    const users = new Set();
+    getTaskList().forEach(task => {
+        getUsersFromTask(task.id).forEach(userId => users.add(userId));
+    });
+    return Array.from(users);
+}
+export function exportAssignments() {
+    return getTaskList().map(task => ({
+        taskId: task.id,
+        userIds: [...getUsersFromTask(task.id)],
+    }));
+}
+export function exportAll() {
+    return {
+        users: exportUsers(),
+        tasks: exportTasks(),
+        assignments: exportAssignments(),
+    };
+}
