10b1371212285dfde620f8a1b7ea71c53d9d758a
upskill228 <upskill228@alunos.ciencias.ulisboa.pt>
2026-01-29 11:59:35 +0000

    ultimo aula

---
+++ dist/utils/GlobalValidators.js
@@ -0,0 +1,10 @@
+export class GlobalValidators {
+    static isValidEmail(email) {
+    }
+    static isNonEmpty(text) {
+    }
+    static isPositiveNumber(value) {
+    }
+    static minLength(text, size) {
+    }
+}
---
+++ src/utils/GlobalValidators.ts
@@ -0,0 +1,18 @@
+export class GlobalValidators {
+    static isValidEmail(email: string): boolean {
+
+    }
+
+    static isNonEmpty(text: string): boolean {
+
+    }
+
+    static isPositiveNumber(value: number): boolean {
+
+    }
+
+    static minLength(text: string, size: number): boolean {
+        
+    }
+
+}
